1108e536e584369083b8df683f95a7739245631c
Kye <kye@apacmediasolutions.com>
2023-05-13 00:15:59 -0400

    name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-  name = '0ptimus-Prime',
+  name = 'optimus-prime-transformers',
   packages = find_packages(exclude=['examples']),
   version = '1.0.1',
   license='MIT',
