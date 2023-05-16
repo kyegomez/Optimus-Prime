f260ad25168beaea6a8ed73e5185e3f22b9ad4df
Kye <kye@apacmediasolutions.com>
2023-05-15 21:57:45 -0400

    relative import

--- optimus_prime/__init__.py
+++ optimus_prime/__init__.py
@@ -5,7 +5,7 @@ if version.parse(torch.__version__) >= version.parse('2.0.0'):
     from einops._torch_specific import allow_ops_in_compiled_graph
     allow_ops_in_compiled_graph()
 
-from optimus_prime import XTransformer, Encoder, Decoder, CrossAttender, Attention, TransformerWrapper, ViTransformerWrapper, ContinuousTransformerWrapper
+from ..optimus_prime import XTransformer, Encoder, Decoder, CrossAttender, Attention, TransformerWrapper, ViTransformerWrapper, ContinuousTransformerWrapper
 
 from optimus_prime.autoregressive_wrapper import AutoregressiveWrapper
 from optimus_prime.nonautoregressive_wrapper import NonAutoregressiveWrapper
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'optimus-prime-transformers',
   packages = find_packages(exclude=['examples']),
-  version = '1.0.1',
+  version = '1.0.7',
   license='MIT',
   description = 'optimus-prime - Pytorch',
   author = 'Kye Gomez',
