edea20dff1d0cbfafffbc59eeb95f24a3f52a6d9
Kye <kye@apacmediasolutions.com>
2023-05-27 01:48:23 -0400

    setup py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'optimus-prime-transformers',
   packages = find_packages(exclude=['examples']),
-  version = '1.1.3',
+  version = '1.1.4',
   license='MIT',
   description = 'optimus-prime - Pytorch',
   author = 'Kye Gomez',
