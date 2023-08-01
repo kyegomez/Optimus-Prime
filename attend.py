37a91fdf0e8741acfe797428e905f4f1fe3e2a5b
Kye <kye@apacmediasolutions.com>
2023-07-31 20:11:57 -0400

    logging

--- attend.py
+++ attend.py
@@ -1,17 +1,24 @@
+import logging
 import pytest
 import torch
 from optimus_prime.attend import Attend
 
+# Set up logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 def test_forward_pass():
+    logger.info("Running forward pass test...")
     model = Attend(dim=512, dim_head=64, q_bucket_size=128, k_bucket_size=128, parallel=False, mixed_precision=False, flash=True)
     q = torch.randn(1, 8, 512, 64)
     k = torch.randn(1, 8, 512, 64)
     v = torch.randn(1, 8, 512, 64)
     out, _ = model(q, k, v)
     assert out.shape == (1, 8, 512, 64)
+    logger.info("Forward pass test passed.")
 
 def test_backward_pass():
+    logger.info("Running backward pass test...")
     model = Attend(dim=512, dim_head=64, q_bucket_size=128, k_bucket_size=128, parallel=False, mixed_precision=False, flash=True)
     q = torch.randn(1, 8, 512, 64, requires_grad=True)
     k = torch.randn(1, 8, 512, 64, requires_grad=True)
@@ -21,8 +28,10 @@ def test_backward_pass():
     assert q.grad is not None
     assert k.grad is not None
     assert v.grad is not None
+    logger.info("Backward pass test passed.")
 
 def test_memory_usage():
+    logger.info("Running memory usage test...")
     model = Attend(dim=512, dim_head=64, q_bucket_size=128, k_bucket_size=128, parallel=False, mixed_precision=False, flash=True)
     q = torch.randn(1, 8, 512, 64)
     k = torch.randn(1, 8, 512, 64)
@@ -31,9 +40,11 @@ def test_memory_usage():
     out, _ = model(q, k, v)
     after_memory = torch.cuda.memory_allocated()
     assert after_memory - before_memory < 1e6  # less than 1MB increase
+    logger.info("Memory usage test passed.")
 
 def test_execution_time():
     import time
+    logger.info("Running execution time test...")
     model = Attend(dim=512, dim_head=64, q_bucket_size=128, k_bucket_size=128, parallel=False, mixed_precision=False, flash=True)
     q = torch.randn(1, 8, 512, 64)
     k = torch.randn(1, 8, 512, 64)
@@ -42,11 +53,14 @@ def test_execution_time():
     out, _ = model(q, k, v)
     end_time = time.time()
     assert end_time - start_time < 1  # less than 1 second
+    logger.info("Execution time test passed.")
 
 def test_error_rate():
+    logger.info("Running error rate test...")
     model = Attend(dim=512, dim_head=64, q_bucket_size=128, k_bucket_size=128, parallel=False, mixed_precision=False, flash=True)
     q = torch.randn(1, 8, 512, 64)
     k = torch.randn(1, 8, 512, 64)
     v = torch.randn(1, 8, 512, 64)
     out, _ = model(q, k, v)
     assert (out != out).sum() == 0  # no NaN values
+    logger.info("Error rate test passed.")
