bb7b102820cddb4ef2f59153157bfb11bba2c26a
Kye <kye@apacmediasolutions.com>
2023-07-31 20:15:48 -0400

    flash attention 2.0

--- attend.py
+++ attend.py
@@ -65,9 +65,64 @@ def test_error_rate():
     assert (out != out).sum() == 0  # no NaN values
     logger.info("Error rate test passed.")
 
+########################## FLASH ATTENTIO 2.0
+def test_forward_pass_flash2():
+    logger.info("Running forward pass test for Flash Attention 2.0...")
+    model = Attend(dim=512, dim_head=64, q_bucket_size=128, k_bucket_size=128, parallel=False, mixed_precision=False, Flash2=True, qk_norm=True, add_zero_kv=True)
+    q = torch.randn(1, 8, 512, 64)
+    k = torch.randn(1, 8, 512, 64)
+    v = torch.randn(1, 8, 512, 64)
+    out, _ = model(q, k, v)
+    assert out.shape == (1, 8, 512, 64)
+    logger.info("Forward pass test for Flash Attention 2.0 passed.")
 
+def test_backward_pass_flash2():
+    logger.info("Running backward pass test for Flash Attention 2.0...")
+    model = Attend(dim=512, dim_head=64, q_bucket_size=128, k_bucket_size=128, parallel=False, mixed_precision=False, Flash2=True, qk_norm=True, add_zero_kv=True)
+    q = torch.randn(1, 8, 512, 64, requires_grad=True)
+    k = torch.randn(1, 8, 512, 64, requires_grad=True)
+    v = torch.randn(1, 8, 512, 64, requires_grad=True)
+    out, _ = model(q, k, v)
+    out.sum().backward()
+    assert q.grad is not None
+    assert k.grad is not None
+    assert v.grad is not None
+    logger.info("Backward pass test for Flash Attention 2.0 passed.")
 
+def test_memory_usage_flash2():
+    logger.info("Running memory usage test for Flash Attention 2.0...")
+    model = Attend(dim=512, dim_head=64, q_bucket_size=128, k_bucket_size=128, parallel=False, mixed_precision=False, Flash2=True, qk_norm=True, add_zero_kv=True)
+    q = torch.randn(1, 8, 512, 64)
+    k = torch.randn(1, 8, 512, 64)
+    v = torch.randn(1, 8, 512, 64)
+    before_memory = torch.cuda.memory_allocated()
+    out, _ = model(q, k, v)
+    after_memory = torch.cuda.memory_allocated()
+    assert after_memory - before_memory < 1e6  # less than 1MB increase
+    logger.info("Memory usage test for Flash Attention 2.0 passed.")
 
+def test_execution_time_flash2():
+    import time
+    logger.info("Running execution time test for Flash Attention 2.0...")
+    model = Attend(dim=512, dim_head=64, q_bucket_size=128, k_bucket_size=128, parallel=False, mixed_precision=False, Flash2=True, qk_norm=True, add_zero_kv=True)
+    q = torch.randn(1, 8, 512, 64)
+    k = torch.randn(1, 8, 512, 64)
+    v = torch.randn(1, 8, 512, 64)
+    start_time = time.time()
+    out, _ = model(q, k, v)
+    end_time = time.time()
+    assert end_time - start_time < 1  # less than 1 second
+    logger.info("Execution time test for Flash Attention 2.0 passed.")
+
+def test_error_rate_flash2():
+    logger.info("Running error rate test for Flash Attention 2.0...")
+    model = Attend(dim=512, dim_head=64, q_bucket_size=128, k_bucket_size=128, parallel=False, mixed_precision=False, Flash2=True, qk_norm=True, add_zero_kv=True)
+    q = torch.randn(1, 8, 512, 64)
+    k = torch.randn(1, 8, 512, 64)
+    v = torch.randn(1, 8, 512, 64)
+    out, _ = model(q, k, v)
+    assert (out != out).sum() == 0  # no NaN values
+    logger.info("Error rate test for Flash Attention 2.0 passed.")
 
 
 
