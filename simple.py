4d65eba52b225c58a00ea581b89437185eb5ea3d
Kye <kye@apacmediasolutions.com>
2023-07-31 20:22:31 -0400

    misplaced q

--- simple.py
+++ simple.py
@@ -1,7 +1,8 @@
 import torch
 from optimus_prime.attend import Attend
 
-model = Attend(dim=512, dim_head=64, heads=64, q_bucket_size=128, k_bucket_size=128, parallel=False, mixed_precision=False, Flash2=True)q = torch.randn(1, 8, 512, 64)
+model = Attend(dim=512, dim_head=64, heads=64, q_bucket_size=128, k_bucket_size=128, parallel=False, mixed_precision=False, Flash2=True)
+q = torch.randn(1, 8, 512, 64)
 k = torch.randn(1, 8, 512, 64)
 v = torch.randn(1, 8, 512, 64)
 out, _ = model(q, k, v)
