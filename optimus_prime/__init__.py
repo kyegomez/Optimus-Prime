75e1abd2e9db3acf106c93e58162310c3dcf8040
Kye <kye@apacmediasolutions.com>
2023-08-05 01:33:08 -0400

    clean up of inits

--- optimus_prime/__init__.py
+++ optimus_prime/__init__.py
@@ -6,4 +6,9 @@ if version.parse(torch.__version__) >= version.parse('2.0.0'):
     allow_ops_in_compiled_graph()
 
 
-# d
+from x_transformers.x_transformers import XTransformer, Encoder, Decoder, CrossAttender, Attention, TransformerWrapper, ViTransformerWrapper, ContinuousTransformerWrapper
+
+from x_transformers.autoregressive_wrapper import AutoregressiveWrapper
+from x_transformers.nonautoregressive_wrapper import NonAutoregressiveWrapper
+from x_transformers.continuous_autoregressive_wrapper import ContinuousAutoregressiveWrapper
+from x_transformers.xl_autoregressive_wrapper import XLAutoregressiveWrapper
